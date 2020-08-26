b8fd5462162c714d38fff26fc27c868b282b99e6
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-08-26 22:52:36 +0600

    setting up the UI

--- app/src/main/java/com/luminous/android/braintrainer/MainActivity.java
+++ app/src/main/java/com/luminous/android/braintrainer/MainActivity.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
     }
 
     public void startGame(View view) {
-
+        startButton.setVisibility(View.INVISIBLE);
     }
 }
