63ba7b27ef3c0fa5e558715149a14fb8bed4c457
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-08-30 14:16:38 +0600

    setting up UI

--- app/src/main/java/com/luminous/android/braintrainer/MainActivity.java
+++ app/src/main/java/com/luminous/android/braintrainer/MainActivity.java
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private Button startButton;
+    private TextView quizTextView;
+    private ArrayList<Integer> answerList = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +22,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startButton = findViewById(R.id.startButton);
+        quizTextView = findViewById(R.id.quizText);
+
+
     }
 
     public void startGame(View view) {
         startButton.setVisibility(View.INVISIBLE);
+    }
+
+    public void quizUISetUp() {
+        int x = new Random().nextInt(99);
+        int y = new Random().nextInt(99);
+
+        int correctAnswerIndex = new Random().nextInt(4);
+
+        quizTextView.setText(MessageFormat.format("{0} + {1}", x, y));
+
+        for (int i = 0; i < 4; i++) {
+            
+        }
+
     }
 }
