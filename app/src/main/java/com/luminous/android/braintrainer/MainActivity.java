92b8bca5d250e59faeace28cc911db2c5ebf6dbb
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-08-30 21:50:00 +0600

    main activity

--- app/src/main/java/com/luminous/android/braintrainer/MainActivity.java
+++ app/src/main/java/com/luminous/android/braintrainer/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
             resultText.setText("Correct!");
             score++;
         } else {
-            resultText.setText("Wrong!!!");
+            resultText.setText("Wrong :(");
         }
 
         totalQuestion++;
